3d7b61746e1fd491b0a3dea1a76541a62f9b17b9
fayca121 <bz.faycal@gmail.com>
2022-10-30 09:18:43 +0100

    add hazelcast cach

--- src/main/java/dz/bououza/CalculatorResource.java
+++ src/main/java/dz/bououza/CalculatorResource.java
@@ -1,5 +1,8 @@
 package dz.bououza;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,11 +16,17 @@ public class CalculatorResource {
     @Inject
     CalculatorService calculatorService;
 
+    @Inject
+    HazelcastInstance hazelcastClient;
+
     @GET
     @Path("sum")
     @Produces(MediaType.TEXT_PLAIN)
     public String sum(@QueryParam("a") Integer a, @QueryParam("b") Integer b) {
-        return String.valueOf(calculatorService.sum(a,b));
+        IMap<String,String> cache=hazelcastClient.getMap("operations-result-cache");
+        String result = cache.getOrDefault(a+"+"+b,String.valueOf(calculatorService.sum(a,b)));
+        cache.putIfAbsent(a+"+"+b,result);
+        return result;
     }
     @GET
     @Path("minus")
